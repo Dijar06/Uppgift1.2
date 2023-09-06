6d4d3ecd32832de783826db86c989da507b3dcdc
Dijar06 <dize06001@utb.vaxjo.se>
2023-09-06 10:25:23 +0200

    Add project files.

---
+++ Uppgift1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.Clear();
+            Console.WriteLine(":(");
+            Console.WriteLine("Din PC has kraschat, datorn kommer restartas.");
+        }
+    }
+}
